f8da97ec42e08e9be892119e99cf5458c0984148
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-14 11:54:46 +0100

    CarForm component: handle name change

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { changeName } from "../store";
 
 export default function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const { name, cost } = useSelector((state) => state.form);
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    dispatch(changeName(value));
+  };
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
