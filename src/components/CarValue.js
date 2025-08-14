23c5dfe9c035d5a205db6abc259a390879731be9
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-14 13:35:56 +0100

    CarValue component: total cost of all cars, also will chaneg with search(show total cost on selected/searched cars)

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector((state) => {
+    const { searchTerm, cars } = state.cars;
+    const filtredCars = searchTerm
+      ? cars.filter((car) =>
+          car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : cars;
+    return filtredCars.reduce((sum, cur) => sum + cur.cost, 0);
+  });
+  return <div className="car-value">Total cost: ${totalCost}</div>;
 }
