00458c8c0d1218ea60394fcc23dbdac6c4933c07
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-14 12:59:30 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CarValue from "./components/CarValue";
 
 export default function App() {
   return (
-    <div>
+    <div className="container is-fuild">
       <CarForm />
       <CarSearch />
       <CarList />
