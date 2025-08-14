8cf93e0a38f8372a47c35097154070918954ef6f
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-14 13:23:00 +0100

    CarList component: filter cars with search term change

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,12 +5,17 @@ import { deleteCar } from "../store/slices/carsSlice";
 export default function CarList() {
   const dispatch = useDispatch();
   const { searchTerm, cars } = useSelector((state) => state.cars);
+  const filtredCars = searchTerm
+    ? cars.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : cars;
 
   const handleCarDelete = (car) => {
     dispatch(deleteCar(car.id));
   };
 
-  const renderedCars = cars.map((car) => {
+  const renderedCars = filtredCars.map((car) => {
     return (
       <div key={car.id} className="panel">
         <p>
