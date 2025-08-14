64d8a0855afd2a33a25adc3d2e48d18a58d24476
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-14 11:42:16 +0100

    wrap App in react-redux provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
