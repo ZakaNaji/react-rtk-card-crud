aa2c83c072f106fbe7b3e7d9fdcf1092551151a7
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-14 12:48:40 +0100

    CarList component: display cars list + handle delete

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteCar } from "../store/slices/carsSlice";
 
 export default function CarList() {
-  return <div>CarList</div>;
+  const dispatch = useDispatch();
+  const { searchTerm, cars } = useSelector((state) => state.cars);
+
+  const handleCarDelete = (car) => {
+    dispatch(deleteCar(car.id));
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
